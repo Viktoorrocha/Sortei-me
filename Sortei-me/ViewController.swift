f7c6d26577ac1bdc713a503b24bdb6eebc7d2350
Viktor Rocha <viktoorrocha@gmail.com>
2017-09-21 23:57:31 -0400

    Fimse

--- Sortei-me/ViewController.swift
+++ Sortei-me/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        
+        
+        var numero = arc4random_uniform(101)
+        legendaResultado.text = String(numero)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
